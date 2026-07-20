57445c41c7588ef5604425536ff22ab4655f252f
alma-98 <alma.budsteddy88@gmail.com>
2026-07-20 18:35:55 +0700

    Dashboard prototype

---
+++ src/pages/Landing/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Workforce Intelligence Platform</h1>
+      <p>Prototype for Medika Nusantara</p>
+      <p>Powered by Rakamin</p>
+    </div>
+  );
+}

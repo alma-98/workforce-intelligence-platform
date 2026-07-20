92f1268d79a15abfc55e8722960808a5bca7688c
alma-98 <alma.budsteddy88@gmail.com>
2026-07-20 20:28:09 +0700

    Connect landing page launch button to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Landing/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
